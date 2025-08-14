487746f9c2c3b84b70a3334ab58318f79791bed4
adarsh-cmtai <adarsh.shahi@completrix.com>
2025-08-14 23:46:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const app = express();
 
 // CORS setup
 app.use(cors({
-  origin:'http://localhost:3000',
+  origin:'http://localhost:3000' || 'https://metro-clap-frontend-code.vercel.app',
   credentials: true
 }));
 
