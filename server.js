8d0246a3a4e075173e120f6e43ed780c6cd23d48
adarsh-cmtai <adarsh.shahi@completrix.com>
2025-08-14 23:52:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -25,13 +25,22 @@ connectDB();
 const app = express();
 
 // CORS setup
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://metro-clap-frontend-code.vercel.app'
+];
+
 app.use(cors({
-  origin: [
-    'http://localhost:3000',
-    'https://metro-clap-frontend-code.vercel.app'
-  ],
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true
 }));
+
 
 
 app.use(express.json());
